3c39044b18e6d2b204f5f848a41f31a6d5ef4479
Matthew Gillingham <me@mattgillingham.com>
2015-09-20 16:50:53 +0900

    Add blog and localization capability

--- config.rb
+++ config.rb
@@ -1,4 +1,38 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :i18n, mount_at_root: false, langs: [:en, :ja]
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  blog.permalink = "{lang}/{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "blog/{lang}/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "article"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
 # Compass
 ###
 
@@ -14,19 +48,19 @@
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -36,9 +70,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+# activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -71,27 +103,4 @@ configure :build do
   # set :http_prefix, "/Content/images/"
 end
 
-# Configuration variables specific to each project
-#------------------------------------------------------------------------
-AWS_BUCKET                      = 'eventacular.net'
-AWS_CLOUDFRONT_DISTRIBUTION_ID  = "E1TSZTD108TO8H"
 
-# Variables: Sent in on CLI by rake task via ENV
-#------------------------------------------------------------------------
-AWS_ACCESS_KEY                  = ENV['AWS_ACCESS_KEY']
-AWS_SECRET                      = ENV['AWS_SECRET']
-
-# https://github.com/fredjean/middleman-s3_sync
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = AWS_BUCKET # The name of the S3 bucket you are targeting. This is globally unique.
-  s3_sync.aws_access_key_id          = AWS_ACCESS_KEY
-  s3_sync.aws_secret_access_key      = AWS_SECRET
-  s3_sync.delete                     = false # We delete stray files by default.
-  s3_sync.region                     = "ap-southeast-1"
-end
-
-activate :cloudfront do |cf|
-  cf.access_key_id                    = AWS_ACCESS_KEY
-  cf.secret_access_key                = AWS_SECRET
-  cf.distribution_id                  = AWS_CLOUDFRONT_DISTRIBUTION_ID
-end
